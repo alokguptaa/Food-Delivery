1c1ebea8e972a8760996d58c8da8995a37764d87
Alok Gupta <alokguptaa786030@gmail.com>
2026-06-30 19:29:27 +0530

    Update useGetItemsByCity.jsx

--- frontend/src/hooks/useGetItemsByCity.jsx
+++ frontend/src/hooks/useGetItemsByCity.jsx
@@ -6,7 +6,7 @@ import { socket } from '../socket.js';
 import { EVENTS } from '../socket/events.js';
 import { updateShopStatusInCity } from "../redux/userSlice";
 
-const serverUrl = "http://localhost:3000";
+const serverUrl = "https://food-delivery-yucy.onrender.com";
 
 const useGetItemsByCity = () => {
 
