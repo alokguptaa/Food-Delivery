3b0c509726be860c7f3e45752a4b6ee405afb593
Alok Gupta <alokguptaa786030@gmail.com>
2026-07-02 13:10:29 +0530

    Refactor cloudinary upload function for clarity

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,29 +1,21 @@
-import { v2 as cloudinary } from "cloudinary";
-import fs from "fs";
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
 
 const uploadOnCloudinary = async (file) => {
     cloudinary.config({
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET,
+        api_secret: process.env.CLOUDINARY_API_SECRET
     });
 
     try {
-        const result = await cloudinary.uploader.upload(file);
-
-        if (fs.existsSync(file)) {
-            fs.unlinkSync(file);
-        }
-
-        return result.secure_url;
+        const result = await cloudinary.uploader.upload(file)
+        fs.unlinkSync(file)
+        return result.secure_url
     } catch (error) {
-        if (fs.existsSync(file)) {
-        fs.unlinkSync(file);
+        fs.unlinkSync(file)
+        console.log(error)
     }
-
-    console.error("CLOUDINARY ERROR:", error);
-    throw error;
-    }
-};
+}
 
 export default uploadOnCloudinary;
