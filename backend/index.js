546eae4ad1b4b972ecfba12f8c4362fe11c63b81
Alok Gupta <alokguptaa786030@gmail.com>
2026-07-03 12:35:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,8 @@ app.set("trust proxy", 1);
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://food-delivery-1dl9-git-main-alokguptaas-projects.vercel.app"
+    "https://food-delivery-1dl9-git-main-alokguptaas-projects.vercel.app",
+    "https://food-delivery.vercel.app"
 ];
 
 
