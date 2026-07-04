11ceba10ab25104be3da1aab5a39787e9a5ca9d8
Alok Gupta <alokguptaa786030@gmail.com>
2026-07-04 12:45:49 +0530

    Update mail.js

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -5,12 +5,17 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
+    port: 587,
     secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+    family: 4,
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
 });
 
 transporter.verify((error, success) => {
