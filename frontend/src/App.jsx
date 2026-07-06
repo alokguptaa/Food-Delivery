71d24b6b8952e777365627475af599a46013ee4e
Alok Gupta <alokguptaa786030@gmail.com>
2026-07-06 16:37:02 +0530

    Add Leaflet integration and marker icon configuration
    Import Leaflet and its CSS, and configure marker icons.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,6 +33,21 @@ import { updateOwnerItemRating } from './redux/ownerSlice.js';
 import { addNotification } from "./redux/notificationSlice.js";
 import { updateAssignedDeliveryBoy, updateRealtimeOrderStatus } from './redux/userSlice.js';
 
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
 
 export const serverUrl = "https://food-delivery-yucy.onrender.com"
 
