f12a8ca3a5b745acc1f3c6da37cbf114cbf1145c
Alok Gupta <alokguptaa786030@gmail.com>
2026-07-06 16:22:01 +0530

    Update CheckOut.jsx

--- frontend/src/pages/CheckOut.jsx
+++ frontend/src/pages/CheckOut.jsx
@@ -165,7 +165,7 @@ return (
                         <IoIosSearch size={18}/>
                     </button>
                     <button className='bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 rounded-lg flex items-center justify-center cursor-pointer' onClick={getCurrentLocation}>
-                        <TbCurrentLocation size={18}/>
+                        <IoIosSearch size={18}/>
                     </button>
                 </div>
                 <div className='rounded-xl border overflow-hidden'>
